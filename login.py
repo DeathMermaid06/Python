363fb31139e3ede4564de96aef6cd6a3ab5e25b3
Mariela Pistasoli <106203272+DeathMermaid06@users.noreply.github.com>
2023-02-22 11:44:16 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,5 @@
+usuario = input("ingrese el usuario")
+password = input("ingrese la contraseña")
+registro_de_usuarios = {"nombre_usuario":usuario, "contrasena":password}
+
+print(registro_de_usuarios)
