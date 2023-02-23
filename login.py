12c68ab9995e65280c9d7e6f7dc6929545c6398e
Mariela Pistasoli <106203272+DeathMermaid06@users.noreply.github.com>
2023-02-23 00:10:12 -0300

    login actualizado
    registro/mostrar/login/guardar

--- login.py
+++ login.py
@@ -1,5 +1,35 @@
-usuario = input("ingrese el usuario")
-password = input("ingrese la contraseña")
-registro_de_usuarios = {"nombre_usuario":usuario, "contrasena":password}
+registro={}
 
-print(registro_de_usuarios)
+def registrar_usuario():
+  usuario=input("Ingrese el usuario: ")
+  while usuario in registro.keys():
+    print("El usuario ya existe, elija otro nombre")
+    break
+  else:
+    contrasena=input("Ingrese la contraseña: ")
+    registro[usuario]=contrasena
+    return (print("El usuario se ha registrado correctamente"))
+
+def mostrar_usuario():
+  print("Los usuarios registrados hasta el momento son: ")
+  for r in registro: 
+    print(f"Usuario:{r} Contraseña:{registro[r]}")
+
+def login_usuario():
+  usuario = input("Ingrese el usuario: ")
+  if usuario in registro.keys():
+    password = input("Ingrese la contraseña: ")
+    if usuario in registro:
+      if password==(registro[usuario]):
+        print("Ha ingresado correctamente")
+      else:
+        print("El password es incorrecto")
+  else:
+    print("El usuario no existe")
+
+def guardar_registro():
+  o_usuarios =open("/usuarios_last.txt", "w")
+  for r in registro:
+    o_usuarios.write(f"Usuario:{r} Contraseña:{registro[r]}")
+  o_usuarios.close()
+
